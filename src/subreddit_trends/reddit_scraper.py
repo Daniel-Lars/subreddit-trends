bbed1d3189cfea7f4d8f3d0dc825a6fe1e5535f7
Daniel Wendel <daniel2wendel@gmail.com>
2025-07-22 14:25:54 +0200

    precommit implemented and applied

--- src/subreddit_trends/reddit_scraper.py
+++ src/subreddit_trends/reddit_scraper.py
@@ -1,17 +1,20 @@
-import praw
 from datetime import datetime
-import pandas as pd 
+
+import pandas as pd
+import praw
+
 
 class RedditScraper:
-    
     def __init__(self):
         self.reddit = praw.Reddit("subreddit_trends_bot1")
-        
-    def get_top_submission(self, subreddit_name, time_filter='week', limit=1)-> pd.DataFrame:
+
+    def get_top_submission(
+        self, subreddit_name, time_filter="week", limit=1
+    ) -> pd.DataFrame:
         """Fetches the top submission(s) from a specified subreddit within a given time filter.
         Parameters:
             subreddit_name (str): Name of the subreddit to fetch submissions from.
-            time_filter (str, optional): Time period to consider for top submissions. 
+            time_filter (str, optional): Time period to consider for top submissions.
                 Valid values are 'all', 'day', 'hour', 'month', 'week', 'year'. Defaults to 'week'.
             limit (int, optional): Number of top submissions to fetch. Defaults to 1.
         Returns:
@@ -30,58 +33,66 @@ class RedditScraper:
                 - num_of_images: Number of images in the post (0 for non-image posts)
                 - upvote_ratio: Upvote ratio of the submission
         """
-        
+
         data = []
-        
-        for submission in self.reddit.subreddit(subreddit_name).top(time_filter=time_filter, limit=limit):
-            
+
+        for submission in self.reddit.subreddit(subreddit_name).top(
+            time_filter=time_filter, limit=limit
+        ):
             # Calculate the number of images in the gallery if it is a gallery post
             # Otherwise, set it to 1 for single image posts
             # and handle other post types accordingly
             if getattr(submission, "is_gallery", False):
-                post_type = 'image_gallery'
-                num_of_images = len(getattr(submission, "gallery_data", {}).get("items", []))
-            elif getattr(submission, "post_hint", "") == 'image':
-                post_type = 'single_image'
+                post_type = "image_gallery"
+                num_of_images = len(
+                    getattr(submission, "gallery_data", {}).get("items", [])
+                )
+            elif getattr(submission, "post_hint", "") == "image":
+                post_type = "single_image"
                 num_of_images = 1
             else:
-                post_type = 'other'
+                post_type = "other"
                 num_of_images = 0
-                
-            data.append({
-                
-                'id': submission.id,
-                'url': submission.url,
-                'permalink': submission.permalink,
-                'subreddit': submission.subreddit.display_name,
-                'author': submission.author.name if submission.author else None,
-                'title': submission.title,
-                'created_utc': datetime.fromtimestamp(submission.created_utc).strftime("%Y-%m-%d %H:%M:%S"),
-                'post_type': post_type, 
-                'score': submission.score,
-                'num_comments': submission.num_comments,
-                'is_gallery': submission.is_gallery,
-                'num_of_images': num_of_images,
-                'upvote_ratio': submission.upvote_ratio,
-            })
-            
-            df = pd.DataFrame(data)  
-            
+
+            data.append(
+                {
+                    "id": submission.id,
+                    "url": submission.url,
+                    "permalink": submission.permalink,
+                    "subreddit": submission.subreddit.display_name,
+                    "author": submission.author.name if submission.author else None,
+                    "title": submission.title,
+                    "created_utc": datetime.fromtimestamp(
+                        submission.created_utc
+                    ).strftime("%Y-%m-%d %H:%M:%S"),
+                    "post_type": post_type,
+                    "score": submission.score,
+                    "num_comments": submission.num_comments,
+                    "is_gallery": submission.is_gallery,
+                    "num_of_images": num_of_images,
+                    "upvote_ratio": submission.upvote_ratio,
+                }
+            )
+
+            df = pd.DataFrame(data)
+
             # Explicit type casting
-            df = df.astype({
-                'id': 'string',
-                'url': 'string',
-                'permalink': 'string',
-                'subreddit': 'string',
-                'author': 'string',
-                'title': 'string',
-                'created_utc': 'datetime64[ns]',
-                'post_type': 'string',
-                'score': 'int64',
-                'num_comments': 'int64',
-                'is_gallery': 'boolean',
-                'num_of_images': 'int64',
-                'upvote_ratio': 'float64',
-            })
-        
-        return df     
+            df = df.astype(
+                {
+                    "id": "string",
+                    "url": "string",
+                    "permalink": "string",
+                    "subreddit": "string",
+                    "author": "string",
+                    "title": "string",
+                    "created_utc": "datetime64[ns]",
+                    "post_type": "string",
+                    "score": "int64",
+                    "num_comments": "int64",
+                    "is_gallery": "boolean",
+                    "num_of_images": "int64",
+                    "upvote_ratio": "float64",
+                }
+            )
+
+        return df
