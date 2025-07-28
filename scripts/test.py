543bf416a7095205b0689b1700401bdce7c89688
Daniel Wendel <daniel2wendel@gmail.com>
2025-07-28 08:47:41 +0200

    test exit code 1

---
+++ scripts/test.py
@@ -0,0 +1,16 @@
+import sys
+
+
+def exit_code_zero():
+    print("Exiting with code 0")
+    sys.exit(0)
+
+
+def exit_code_one():
+    print("Exiting with code 1")
+    sys.exit(1)
+
+
+if __name__ == "__main__":
+    # Test exit with code 1
+    exit_code_one()
