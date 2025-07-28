cb8beb1fa4b713859a3efcaefe1c4a879b2c59db
Daniel Wendel <daniel2wendel@gmail.com>
2025-07-28 09:02:50 +0200

    typer app added

--- src/subreddit_trends/cli/test.py
+++ src/subreddit_trends/cli/test.py
@@ -1,15 +1,21 @@
 import sys
 
+import typer
 
+app = typer.Typer()
+
+
+@app.command()
 def exit_code_zero():
     print("Exiting with code 0 - everything should be fine, right?")
     sys.exit(0)
 
 
+@app.command()
 def exit_code_one():
     print("Causing expected failure and exiting with code 1")
     sys.exit(1)
 
 
 if __name__ == "__main__":
-    exit_code_zero()
+    app()
